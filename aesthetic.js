09cad20c2753af119299231d2227bf34334392b6
Victor Willyams <victor.willyams@cortexo.com>
2015-08-18 03:58:01 +0000

    No new functionality, just committing what I have as I just hit a wall

--- aesthetic.js
+++ aesthetic.js
@@ -17,14 +17,25 @@ function toFullWidth(chars) {
 }
 
 function modifyElementText(element, find, replace){
-  if(element.innerText){
+  var found = false;
+  if (element.innerText && element.innerText.indexOf(find) !== -1) {
     element.innerText = element.innerText.replace(find, replace);
+    found = true;
   }
-  if(element.value){
+  if (element.value && element.value.indexOf(find) !== -1) {
     element.value = element.value.replace(find, replace);
+    found = true;
   }
-  // Need to reselect the contents of the element so we can hijack the clipboard
-  element.select();
+  if (found) {
+    // Need to reselect the contents of the element so we can hijack the clipboard
+    element.select();
+  } else {
+    // no good
+    // Flow down the DOM tree - used for Facebook comments box and other areas where the DOM isn't quite as sensible as we'd like
+   // for (var i = 0; i < element.childNodes.length; i++){
+     // modifyElementText(element.childNodes[i], find, replace);
+    //}
+  }    
 }
 
 
@@ -38,10 +49,16 @@ document.body.addEventListener("keydown", function (event) {
   // Get the selected text
   var selection = window.getSelection();
   var text = selection.toString();
-  var selectedElement = selection.focusNode.childNodes[selection.focusOffset];
 
-  if(text.length) {    
-    modifyElementText(selectedElement, text, toFullWidth(text));
+  if (text.length) {    
+    var selectedElement = selection.focusNode.childNodes[selection.focusOffset];
+    if(selectedElement){
+      modifyElementText(selectedElement, text, toFullWidth(text));
+    } else {
+      // nope this isn't a good approach
+      // Brute force find+replace guarantees execution even if selection behaviour is overridden by website
+      // modifyElementText(document.activeElement, text, toFullWidth(text));
+    }
     // Hijack the clipboard to get around security measures
     document.execCommand("copy");
     document.execCommand("paste");
