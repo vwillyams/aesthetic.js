fc4f5e290623e6da035cfec1712dc46584643c36
vwillyams <v.willyams@gmail.com>
2015-08-19 21:04:08 +1200

    ES6: Beginning to improve doc and switch to ES6, some errors introduced

--- aesthetic.js
+++ aesthetic.js
@@ -1,38 +1,56 @@
 "use strict";
 
-function toFullWidth(chars) {
-  var aesthetic = '';
-  for(var i=0, l=chars.length; i<l; i++) {
-    var c = chars[i].charCodeAt(0);
+const spaceChar = 0x0020;
+const asciiEndChar = 0x007F;
+const fullWidthCharOffset = 0xFEE0;
+const ideographicSpaceChar = 0x3000;
 
+function toFullWidth(chars) {
+  let aesthetic = '';
+  for(var c of chars) {
     // don't convert non-ASCII
-    if (c != 0x0020 && c <= 0x007F) {
-      c += 0xFEE0;
-    } else if(c == 0x0020) { // replace space with...
-      c = 0x3000; // ideographic space 
+    if (c != spaceChar && c <= asciiEndChar) {
+      c += fullWidthCharOffset;
+    } else if(c == spaceChar) { // replace space with...
+      c = ideographicSpaceChar; // ideographic space
     }
     aesthetic += String.fromCharCode(c);
   }
   return aesthetic;
 }
 
+// Selects an element, removing all other selections
 function selectElement(element){
-  var range = document.createRange();
+  let range = document.createRange();
   range.selectNode(element);
-  var selection = window.getSelection();
+
+  let selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+}
+
+// Creates a copy of a Range object (in the likely case that the original range has expired)
+// and sets window.selection to contain that range
+function setSelection(selectionRange){
+  let range = document.createRange();
+  range.setStart(selectionRange.startContainer, selectionRange.startOffset);
+  range.setEnd(selectionRange.endContainer, selectionRange.endOffset);
+
+  let selection = window.getSelection();
   selection.removeAllRanges();
   selection.addRange(range);
 }
 
 function modifyElementText(element, find, replace){
-  var foundElement;
+  let foundElement;
   if (element.type == 'textarea' || element.type == 'input'){
     foundElement = element;
   }
   else if (element.nodeType == 3) {
     foundElement = element.parentNode;
   }
-  var found = false;
+
+  let found = false;
   if (foundElement) {
     if (foundElement.innerText && foundElement.innerText.indexOf(find) !== -1) {
       foundElement.innerText = foundElement.innerText.replace(find, replace);
@@ -42,13 +60,14 @@ function modifyElementText(element, find, replace){
       found = true;
     }
   }
+
   if (found) {    
     // Need to reselect the contents of the element so we can hijack the clipboard
       selectElement(foundElement);
   } else {
     // Flow down the DOM tree - used for Facebook comments box and other areas where the DOM isn't quite as sensible as we'd like
-    for (var i = 0; i < element.childNodes.length; i++) {
-      modifyElementText(element.childNodes[i], find, replace);
+    for (var childNode in element.childNodes) {
+      modifyElementText(childNode, find, replace);
     }
   }
 }
@@ -60,24 +79,21 @@ document.body.addEventListener("keydown", function (event) {
     return;
   }
 
-  // Get the selected text
-  var selection = window.getSelection();
-  var originalRange = selection.getRangeAt(0);
-  var text = selection.toString();
+  // Get the selected text and store the selection range
+  let selection = window.getSelection();
+  // TODO this works but should be better
+  let originalRange = selection.getRangeAt(0);
+  let text = selection.toString();
 
   if (text.length) {    
-    var selectedElement = selection.focusNode.childNodes[selection.focusOffset];
+    let selectedElement = selection.focusNode.childNodes[selection.focusOffset];
     if (!selectedElement) {
       selectedElement = document.activeElement;
     }
     modifyElementText(selectedElement, text, toFullWidth(text));
     // Hijack the clipboard to get around security measures
     document.execCommand("copy");
-    var range = document.createRange();
-    range.setStart(originalRange.startContainer, originalRange.startOffset);
-    range.setEnd(originalRange.endContainer, originalRange.endOffset);
-    selection.removeAllRanges();
-    selection.addRange(range);
+    setSelection(originalRange);
     document.execCommand("paste");
   }
 }, false);
