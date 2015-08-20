ab19bc3531b783f32b8036dc72b1344e411c5401
vwillyams <v.willyams@gmail.com>
2015-08-20 20:21:37 +1200

    clipboard-safety: the actual safety bit is not going but I am no longer inline replacing text, so that is a start

--- aesthetic.js
+++ aesthetic.js
@@ -20,64 +20,34 @@ function toFullWidth(chars) {
   return aesthetic;
 }
 
+function addBuffer(element) {
+  var node = document.createElement('textarea');
+  element.appendChild(node);
+  return node;
+}
+
 // Selects an element, removing all other selections
 function selectElement(element){
-  let range = document.createRange();
-  range.selectNode(element);
-
   let selection = window.getSelection();
   selection.removeAllRanges();
-  selection.addRange(range);
+  element.select();
 }
 
 // Creates a copy of a Range object (in the likely case that the original range has expired)
 // and sets window.selection to contain that range
-function setSelection(selectionRange){
-  let range = document.createRange();
-  range.setStart(selectionRange.startContainer, selectionRange.startOffset);
-  range.setEnd(selectionRange.endContainer, selectionRange.endOffset);
-
+function setSelection(selectionRanges){
   let selection = window.getSelection();
   selection.removeAllRanges();
-  selection.addRange(range);
-}
 
-function modifyElementText(element, find, replace){
-  let foundElement;
-  if (element.type == 'textarea' || element.type == 'input'){
-    foundElement = element;
-  }
-  else if (element.nodeType == 3) {
-    foundElement = element.parentNode;
-  }
+  for (var i = 0; i < selectionRanges.length; i++) {
+    let selectionRange = selectionRanges[i];
 
-  let found = false;
-  if (foundElement) {
-    if (foundElement.innerText && foundElement.innerText.indexOf(find) !== -1) {
-      foundElement.innerText = foundElement.innerText.replace(find, replace);
-      found = true;
-    } else if (foundElement.value && foundElement.value.indexOf(find) !== -1) {
-      foundElement.value = foundElement.value.replace(find, replace);
-      found = true;
-    }
-  }
+    let range = document.createRange();
+    range.setStart(selectionRange.startContainer, selectionRange.startOffset);
+    range.setEnd(selectionRange.endContainer, selectionRange.endOffset);
 
-  if (!found) {
-    // Flow down the DOM tree - used for Facebook comments box and other areas where the DOM isn't quite as sensible as we'd like
-    for (var i = 0; i < element.childNodes.length; i++) {
-      modifyElementText(element.childNodes[i], find, replace);
-    }
+    selection.addRange(range);
   }
-}
-
-function createClipboardBuffer() {
-  var node = document.createElement('input');
-  node.setAttribute('type', 'hidden')
-  document.body.appendChild(node);
-  window.getSelection().removeAllRanges();
-  node.select();
-  document.execCommand('paste');
-  return node;
 }
 
 document.body.addEventListener('keydown', function (event) {
@@ -90,25 +60,41 @@ document.body.addEventListener('keydown', function (event) {
   // Get the selected text and store the selection range
   let selection = window.getSelection();
   // TODO this works but should be better
-  let originalRange = selection.getRangeAt(0);
+  var originalRanges = [];
+  for (var i = 0; i < selection.rangeCount; i++) {
+    originalRanges.push(selection.getRangeAt(i));
+  }
+  console.log(originalRanges);
   let text = selection.toString();
 
-  var clipboardBuffer = createClipboardBuffer();
+  //let clipboardBuffer = addBuffer();
+  //selectElement(clipboardBuffer);
+  //document.execCommand('paste');
+  //console.log(clipboardBuffer);
 
-  if (text.length) {    
-    let selectedElement = selection.focusNode.childNodes[selection.focusOffset];
-    if (!selectedElement) {
-      selectedElement = document.activeElement;
-    }
-    modifyElementText(selectedElement, text, toFullWidth(text));
+  if (text.length) {
+
+    var selectedElement = selection.focusNode.childNodes[selection.focusOffset];
+
+    let textBuffer = addBuffer(selectedElement.parentNode);
+    textBuffer.innerText = toFullWidth(text);
+    selectElement(textBuffer);
+    document.execCommand('copy');
+    selectedElement.parentNode.removeChild(textBuffer);
+
     // Hijack the clipboard to get around security measures
-    setSelection(originalRange);
-    document.execCommand('copy');
+    selectElement(selectedElement);
+    console.log(selection);
     document.execCommand('paste');
+    setSelection(originalRanges);
+    console.log(selection);
+    document.execCommand('paste');
+
     // Restore the original clipboard state
-    clipboardBuffer.select();
-    document.execCommand('copy');
-    selection.removeAllRanges();
-    document.body.removeChild(clipboardBuffer);
+//    clipboardBuffer.select();
+//    document.execCommand('copy');
+//    selection.removeAllRanges();
+
   }
+  //document.body.removeChild(clipboardBuffer);
 }, false);
